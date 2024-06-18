4b9e742a18b98f20433e17b6ff02fa9fa35cc53d
Admin <admin@Admins-MacBook-Pro.local>
2024-06-18 06:44:15 -0400

    -___-

--- contracts/tables.hpp
+++ contracts/tables.hpp
@@ -1,11 +1,11 @@
 #pragma once
 
 struct [[eosio::table]] stat {
-  eosio::asset    		supply;
-  eosio::asset 				max_supply;
-  eosio::name 				issuer;
+	eosio::asset    		supply;
+	eosio::asset 				max_supply;
+	eosio::name 				issuer;
 
-  uint64_t primary_key()const { return supply.symbol.code().raw(); }
+	uint64_t primary_key()const { return supply.symbol.code().raw(); }
 };
 typedef eosio::multi_index< "stat"_n, stat > stat_table;
 
@@ -16,7 +16,7 @@ struct [[eosio::table, eosio::contract(CONTRACT_NAME)]] farms {
 
 	uint64_t primary_key() const { return farm_name.value; }
 };
-using farms_table = eosio::multi_index< "farms"_n, farms>;	
+using farms_table = eosio::multi_index< "farms"_n, farms>;
 
 
 struct [[eosio::table, eosio::contract(CONTRACT_NAME)]] points {
@@ -25,7 +25,7 @@ struct [[eosio::table, eosio::contract(CONTRACT_NAME)]] points {
 
 	uint64_t primary_key() const { return wallet.value; }
 };
-using points_table = eosio::multi_index< "points"_n, points>;	
+using points_table = eosio::multi_index< "points"_n, points>;
 
 
 struct [[eosio::table, eosio::contract(CONTRACT_NAME)]] state {
@@ -33,7 +33,7 @@ struct [[eosio::table, eosio::contract(CONTRACT_NAME)]] state {
 	uint64_t  											partner_fee_1e6;
 
 	EOSLIB_SERIALIZE(state, (accepted_tokens)
-													(partner_fee_1e6)
-					)
+	                 (partner_fee_1e6)
+	                )
 };
-using state_singleton = eosio::singleton<"state"_n, state>; 
+using state_singleton = eosio::singleton<"state"_n, state>;
